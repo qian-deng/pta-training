2161efc8bfade5fb3d343e8ffdcb61de04c898ad
SuqianMHT <66798032+SuqianMHT@users.noreply.github.com>
2022-07-15 14:31:30 -0400

    add checking conflict fun' '

--- Week3/git-merge.py
+++ Week3/git-merge.py
@@ -3,7 +3,7 @@ import os
 from git import Repo
 
 
-WORK_PATH = r'/Users/yibozhang/pta-training/Week3'
+WORK_PATH = os.getcwd()
 
 # Merge branch name, FROM_BRANCH -> TO_BRANCH
 FROM_BRANCH = 'remotes/origin/create-merge-conflict-1'
@@ -14,7 +14,7 @@ REPO_NAME = "pta-training"
 
 def autoMerge(workpath, repo_name, fromBranch, toBranch):
 
-    if(os.path.isdir(workpath) == False):
+    if(os.path.join(workpath, repo_name) == False):
         print("Error: Folder not exist, tool exit: " + workpath)
         exit()
 
@@ -35,7 +35,6 @@ def autoMerge(workpath, repo_name, fromBranch, toBranch):
     print('end  merge <<< ' + repo_name + '\n')
 
     git = repo.git
-    remoteVer = git.rev_parse("@{u}")
     print("Remote Current Rev Parse:" + remoteVer)
 
 
@@ -53,19 +52,26 @@ def branch_merge(repo, from_branch, to_branch):
 
     # merge branch
     print('git merge ' + from_branch)
-    git.merge(from_branch)
+    git.merge_tree(from_branch, to_branch)
 
     # push merged branch to remote
     print('git push ' + to_branch)
     git.push('--progress', 'origin', to_branch)
+    
+def git_conflicts(Work_Path):
+    repo = git.Repo(Work_Path)
+    status_git = repo.git.status(porcelain=True).split()
+
+    if "UU" in status_git:
+        return True
+
+    return False
 
 
-
-def main():
+    
+if __name__ == '__main__':
+    
     print('Auto Git Merge Tool... \n')
     autoMerge(WORK_PATH, REPO_NAME, FROM_BRANCH, TO_BRANCH)
-
-
-if __name__ == '__main__':
-    main()
+    git_conflicts(WORK_PATH)
 
